b46f96f2f7ad4d7f6ad8733fb0ee989837dffd04
plogerais <53569137+plogerais@users.noreply.github.com>
2019-12-02 17:31:09 +0100

    Start project : Add info on author

--- vulture/__init__.py
+++ vulture/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for Vulture."""
 
-from .__version__ import __author__, __email__, __version__  # noqa: F401
+__author__ = """Pierre Logerais"""
+__email__ = 'plogerais@ipsl.fr'
+__version__ = '0.0.1'
 
 from .wsgi import application  # noqa: F401
